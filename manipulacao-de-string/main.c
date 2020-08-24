b3efebe5437e287447452296b132b5b68bfa10cb
Yuri Siman <yurisimannogueira@gmail.com>
2020-08-24 00:03:25 -0300

    Projeto manipulacao-de-string

---
+++ manipulacao-de-string/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    char string1[50], string2[50], caracter;
+    int cont = 0;
+    
+    printf("\nDigite a primeira palavra:");
+    gets(string1);
+    
+    printf("\nDigite a segunda palavra:");
+    gets(string2);
+    
+    printf("\nConcatenando a segunda palavra com a primeira");
+    strcat(string1, string2);
+    printf("\nResultado da primeira palavra: %s", string1);
+    
+    printf("\n\nExibindo as duas palavras em ordem alfabética - Resultado numérico obtido: %d", strcmp(string1, string2));
+    strcmp(string1, string2) < 0 ? printf("\n%s\n%s", string1, string2) : printf("\n%s\n%s", string2, string1);
+    
+    printf("\n\nSubstituindo a segunda palavra pela primeira");
+    strcpy(string2, string1);
+    printf("\nResultado da segunda palavra: %s", string2);
+    
+    printf("\n\nSubstituindo um caracter por um '*'\nDigite um caracter para fazer a troca na primeira palavra:");
+    scanf("%c", &caracter);
+    for(int i = 0; i < 50; i++){
+        string1[i] == toupper(caracter) || string1[i] == tolower(caracter) ? string1[i] = '*' : string1[i];
+    }
+    printf("\nResultado da primeira palavra: %s", string1);
+    
+    printf("\n\nExibindo o tamanho da primeira palavra: %d caracteres", strlen(string1));
+    
+    printf("\n\nExibindo o tamanho da segunda palavra, sem utilizar nenhuma função da biblioteca string.h:");
+    for(int i = 0; i < 50; i++){
+        if(string2[i] == '\0') break;
+            cont++;
+    }
+    printf(" %d caracteres", cont);
+    
+    return 0;
+}
